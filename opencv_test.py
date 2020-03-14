59fafe09a77b3bb19815c4fe7fc5636b99138174
Michael Li <mlli1@asu.edu>
2020-03-13 20:54:04 -0700

    Testing with a Simple Blob Detector

--- opencv_test.py
+++ opencv_test.py
@@ -2,12 +2,37 @@ import cv2
 import numpy as np
 
 # Read image
-im = cv2.imread("15.jpg", cv2.IMREAD_GRAYSCALE)
+im = cv2.imread('data/images/eggs1.png', cv2.IMREAD_GRAYSCALE)
 
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
 
-detector = cv2.SimpleBlobDetector_create(params)
+# Change thresholds
+params.minThreshold = 100
+params.maxThreshold = 200
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 1000
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.1
+
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.95
+    
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.0001
+
+# Create a detector with the parameters
+ver = (cv2.__version__).split('.')
+if int(ver[0]) < 3 :
+	detector = cv2.SimpleBlobDetector(params)
+else : 
+	detector = cv2.SimpleBlobDetector_create(params)
 
 # Detect blobs.
 keypoints = detector.detect(im)
@@ -19,5 +44,5 @@ keypoints = detector.detect(im)
 im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # Show blobs
-cv2.imshow("Keypoints", im_with_keypoints)
+cv2.imshow('Eggs', im_with_keypoints)
 cv2.waitKey(0)
