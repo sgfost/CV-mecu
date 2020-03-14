ffcf0a03dd2fbda06c20d07749b26e14ed00ed1b
Michael Li <mlli1@asu.edu>
2020-03-13 20:38:54 -0700

    Testing with a Simple Blob Detector

---
+++ opencv_test.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+# Read image
+im = cv2.imread("15.jpg", cv2.IMREAD_GRAYSCALE)
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+detector = cv2.SimpleBlobDetector_create(params)
+
+# Detect blobs.
+keypoints = detector.detect(im)
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+# the size of the circle corresponds to the size of blob
+
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show blobs
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
