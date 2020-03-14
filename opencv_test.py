c44233458d76bcd3e7b2081c8a512726fc400a77
Michael Li <mlli1@asu.edu>
2020-03-13 20:58:34 -0700

    Works with just dots - doesn't work with eggs, as they stack closely together

--- opencv_test.py
+++ opencv_test.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # Read image
 im = cv2.imread('data/images/eggs1.png', cv2.IMREAD_GRAYSCALE)
+im2 = cv2.imread('15.png', cv2.IMREAD_GRAYSCALE)
 
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
@@ -34,15 +35,20 @@ if int(ver[0]) < 3 :
 else : 
 	detector = cv2.SimpleBlobDetector_create(params)
 
+detector2 = cv2.SimpleBlobDetector_create(params)
+
 # Detect blobs.
 keypoints = detector.detect(im)
+keypoints2 = detector2.detect(im2)
 
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
 # the size of the circle corresponds to the size of blob
 
 im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+im_with_keypoints2 = cv2.drawKeypoints(im2, keypoints2, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # Show blobs
 cv2.imshow('Eggs', im_with_keypoints)
+cv2.imshow('Eggs', im_with_keypoints2)
 cv2.waitKey(0)
